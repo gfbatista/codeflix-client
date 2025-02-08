ffd0ad0610b4a1ea33086f7dfa6c99db510183fb
gfbatista <gilbertoferrari@gmail.com>
2025-02-08 13:10:44 -0300

    chore: changing description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,7 @@ export default function Home() {
           <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">MAID</h1>
 
           <p className="text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl">
-            Geralt of Rivia, a solitary monster hunter, struggles to find his
-            place in a world where people often prove more wicked than beasts.
+          Put the description here
           </p>
         </div>
 
