dda988f87362a77d85f4b4ae5cb7216c2e387aad
gfbatista <gilbertoferrari@gmail.com>
2025-02-08 13:05:44 -0300

    feat: add header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header
+      className={`
+        fixed top-0 z-50 flex w-full items-center justify-between  px-4 py-4 transition-all lg:px-10 lg:py-6 `}
+    >
+      <div className="flex items-center space-x-2 md:space-x-8">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="netflix"
+          width={120}
+          height={120}
+        />
+        <ul className="hidden md:flex md:space-x-4">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
