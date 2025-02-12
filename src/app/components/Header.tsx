042e55e5dab76b0bbf0f5e9a41533eac6f94b44d
gfbatista <gilbertoferrari@gmail.com>
2025-02-12 09:53:27 -0300

    refactor: page components

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,25 +1,24 @@
+'use client';
 import React from 'react';
+import { useScroll } from '../hooks/useScroll';
+import { Logo } from './Logo';
+import { NavLinks } from './NavLinks';
+import { UserProfile } from './UserProfile';
 
 export default function Header() {
+  const isScrolled = useScroll();
+
   return (
     <header
-      className={`
-        fixed top-0 z-50 flex w-full items-center justify-between  px-4 py-4 transition-all lg:px-10 lg:py-6 `}
+      className={`${isScrolled && 'bg-black'}
+      fixed top-0 z-50
+      flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}
     >
-      <div className="flex items-center space-x-2 md:space-x-8">
-        <img
-          src="https://rb.gy/ulxxee"
-          alt="netflix"
-          width={120}
-          height={120}
-        />
-        <ul className="hidden md:flex md:space-x-4">
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+      <div className='flex items-center space-x-2 md:space-x-8'>
+        <Logo />
+        <NavLinks />
       </div>
+      <UserProfile />
     </header>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <body className='bg-[#141414] text-white scrollbar-hide'>{children}</body>
     </html>
   );
 }
